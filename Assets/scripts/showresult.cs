4d6781bd83909540d55b7e28bbfa6ded3a95d500
mous494 <mous494@gmail.com>
2016-07-13 15:56:21 +0900

    最終版

--- Assets/scripts/showresult.cs
+++ Assets/scripts/showresult.cs
@@ -5,13 +5,36 @@ public class showresult : MonoBehaviour {
 
 
 	public Text txt;
+	public Text highscore_text;
+	private float highscore;
+	private string key = "HIGH_SCORE";
 	// Use this for initialization
 	void Start () {
+
 
         Cursor.visible = true;
 		txt.text = "Distance:"+ distanceText.distance;
 
-		
+
+		//ハイスコアの処理
+		highscore = PlayerPrefs.GetFloat (key, -1.0F);
+		//値が無いとき
+		if (highscore == -1.0F) {
+			highscore_text.text = "HIGH SCORE:NONE" ;
+
+			highscore = distanceText.distance;
+			PlayerPrefs.SetFloat (key, highscore);
+		} else if (distanceText.distance < highscore) {
+			//ハイスコア更新
+			highscore_text.text = "HIGH SCORE:" + highscore;
+			highscore = distanceText.distance;
+			PlayerPrefs.SetFloat (key, highscore);
+			highscore_text.color = Color.red;
+		} else {
+		//ハイスコア更新なし
+			highscore_text.text = "HIGH SCORE:" + highscore;		
+		}
+			
 	}
 	
 	// Update is called once per frame
